3dbe963f0c677c92e658f6598d1e3e7254506d5b
ktafakkori <k.tafakkori@gmail.com>
2022-11-03 15:40:59 +0330

    Update

--- examples/Workflow_Optimization/Reading_Excel_Data/read.py
+++ examples/Workflow_Optimization/Reading_Excel_Data/read.py
@@ -1,10 +1,13 @@
 from feloopy import *
+import os
 
 I = range(4)
 M = range(4)
 T = range(2)
 J = range(3)
 
-par_h = data("data.xlsx", [I,M,T,J], 2,2,['i','m','t','j'],"h") 
+data_file = os.path.join(sys.path[0], "data.xlsx")
 
-par_p = data("data.xlsx", [I,T,J], 1,2,['i','t','j'],"p")
+par_h = data(data_file, [I,M,T,J], 2,2,['i','m','t','j'],"h") 
+
+par_p = data(data_file, [I,T,J], 1,2,['i','t','j'],"p")
