60426e3fd9091b31dc23fc8e14c09aa5d02303d0
ktafakkori <k.tafakkori@gmail.com>
2023-04-04 19:47:19 +0430

    Update

--- examples/Combinatorial_Optimization/assignment.py
+++ examples/Combinatorial_Optimization/assignment.py
@@ -1,7 +1,7 @@
 from feloopy import *
 
 # Environment
-m = model('exact', 'ap', 'ortools')
+m = model('exact', 'ap', 'gurobi')
 
 # Sets
 I = m.set(3)  # Agents
@@ -26,7 +26,7 @@ for i in I:
 for j in J:
     m.con(sum(x[i, j] for i in I) == 1)
 
-m.sol(['min'], 'scip', log=False)
+m.sol(['min'], 'gurobi', log=True)
 m.inf()
 m.dis_obj()
 m.dis_status()
