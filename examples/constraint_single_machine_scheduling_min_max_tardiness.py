a882cbbc3fc95e42ed66b25466012fa1eec1fe02
ktafakkori <k.tafakkori@gmail.com>
2023-06-20 19:00:31 +0330

    revised files and added examples.

--- examples/constraint_single_machine_scheduling_min_max_tardiness.py
+++ examples/constraint_single_machine_scheduling_min_max_tardiness.py
@@ -4,12 +4,16 @@ m = target_model('constraint', 'single_machine_scheduling', 'cplex_cp', key=0)
 
 J = m.set(10)
 due_date = m.uniform(75, 200, [J])
+release_time = m.uniform(1,50, [J])
 priority_weights = m.uniformint(1,5, [J])
 process_interval = m.cevar('job', J, [[m.uniformint(10,40), None, None] for j in J])
-
-m.obj(sum((m.end_of(process_interval[j]) > due_date[j])*priority_weights[j] for j in J))
+max_lateness = m.ivar('lateness')
+m.obj(max_lateness)
+for j in J:
+    m.con(m.start_of(process_interval[j])>=release_time[j])
+for j in J:
+    m.con(m.max([0,m.end_of(process_interval[j])-due_date[j]])<=max_lateness)
 m.con(m.forbid_overlap([process_interval[j] for j in J]))
-
 m.sol(['min'], 'cplex')
 
 m.report()
