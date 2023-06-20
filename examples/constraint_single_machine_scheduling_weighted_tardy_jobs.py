3e54c114437c9568bdb2e9aaf033026f19e8246d
ktafakkori <k.tafakkori@gmail.com>
2023-06-20 18:41:57 +0330

    added examples.

---
+++ examples/constraint_single_machine_scheduling_weighted_tardy_jobs.py
@@ -0,0 +1,14 @@
+from feloopy import *
+
+m = target_model('constraint', 'single_machine_scheduling', 'cplex_cp', key=0)
+
+J = m.set(10)
+due_date = m.uniform(75, 200, [J])
+priority_weights = m.uniformint(1,5, [J])
+process_interval = m.cevar('job', list(J), [[m.uniformint(10,40), None, None] for j in J])
+
+m.obj(sum((m.end_of(process_interval[j]) > due_date[j])*priority_weights[j] for j in J))
+m.con(m.forbid_overlap([process_interval[j] for j in J]))
+m.sol(['min'], 'cplex')
+
+m.report()
