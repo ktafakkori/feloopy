a6db43c3c6da77edc2be0a7569200f696023ff31
ktafakkori <k.tafakkori@gmail.com>
2023-06-20 19:35:22 +0330

    revised and updated files.

---
+++ examples/constraint_single_machine_scheduling_total_weighted_completion_time_precedence.py
@@ -0,0 +1,25 @@
+from feloopy import *
+
+m = target_model('constraint', 'single_machine_scheduling', 'cplex_cp', key=0)
+
+J = m.set(10)
+
+due_date = m.uniform(75, 200, [J])
+release_time = m.uniform(1,50, [J])
+priority_weights = m.uniformint(1,5, [J])
+precedences = list()
+for j in J:
+    if j>=len(J)/2:
+        precedences.append((m.uniformint(0,len(J)/2-1), j))
+
+process_interval = m.cevar('job', J, [[m.uniformint(10,40), None, None] for j in J])
+
+m.obj(sum([m.end_of(process_interval[j])*priority_weights[j] for j in J])) 
+
+m.con(m.forbid_overlap([process_interval[j] for j in J]))
+
+m.con([m.prec_end_before_start(process_interval[i],process_interval[j]) for [i,j] in precedences])
+
+m.sol(['min'], 'cplex')
+
+m.report()
