347d9986a18b57342944e472dda6c0c3db3d95a3
ktafakkori <k.tafakkori@gmail.com>
2023-06-27 10:05:13 +0330

    added documentation.

---
+++ docs/conf.py
@@ -0,0 +1,13 @@
+# -- Project information -----------------------------------------------------
+
+project = 'FelooPy'
+copyright = '2023, Keivan Tafakkori'
+author = 'Keivan Tafakkori'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+Welcome to FelooPy's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   
