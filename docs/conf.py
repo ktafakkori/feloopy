63fe9d840a70df880fa13bece925970290bb9835
ktafakkori <k.tafakkori@gmail.com>
2023-06-27 18:57:15 +0330

    revised files.

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,29 @@ author = 'Keivan Tafakkori'
 
 # -- General configuration ---------------------------------------------------
 
-extensions = []
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinxext.rediraffe",
+    "sphinx_design",
+    "sphinx_copybutton",
+    "autoapi.extension",
+    "_extension.gallery_directive",
+    # For extension examples and demos
+    "ablog",
+    "jupyter_sphinx",
+    "matplotlib.sphinxext.plot_directive",
+    "myst_nb",
+    "sphinxcontrib.youtube",
+    # "nbsphinx",  # Uncomment and comment-out MyST-NB for local testing purposes.
+    "numpydoc",
+    "sphinx_togglebutton",
+    "jupyterlite_sphinx",
+    "sphinx_favicon",
+]
 
 # -- Options for HTML output -------------------------------------------------
 
