ff16d5a78cac7cc8f8217b1d81859a06b70be921
ktafakkori <k.tafakkori@gmail.com>
2023-07-06 13:04:14 +0330

    revised files.

--- examples/exact_resource_constrained_project_scheduling.py
+++ examples/exact_resource_constrained_project_scheduling.py
@@ -1,3 +1,13 @@
+'''
+ # @ Author: Keivan Tafakkori
+ # @ Created: 2023-06-21
+ # @ Modified: 2023-07-06
+ # @ Contact: https://www.linkedin.com/in/keivan-tafakkori/
+ # @ Github: https://github.com/ktafakkori
+ # @ Website: https://ktafakkori.github.io/
+ # @ Copyright: 2023. MIT License. All Rights Reserved.
+ '''
+
 from feloopy import *
 
 # Credit: https://python-mip.readthedocs.io/en/latest/examples.html
--- examples/exact_single_machine_scheduling_problem_set_based.py
+++ examples/exact_single_machine_scheduling_problem_set_based.py
@@ -1,3 +1,13 @@
+'''
+ # @ Author: Keivan Tafakkori
+ # @ Created: 2023-06-18
+ # @ Modified: 2023-07-06
+ # @ Contact: https://www.linkedin.com/in/keivan-tafakkori/
+ # @ Github: https://github.com/ktafakkori
+ # @ Website: https://ktafakkori.github.io/
+ # @ Copyright: 2023. MIT License. All Rights Reserved.
+ '''
+
 from feloopy import *
 
 #Environment
