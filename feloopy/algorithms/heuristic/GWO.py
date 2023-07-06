da4c18bb76d018f228a1b62ec14b1bf6ba93c916
ktafakkori <k.tafakkori@gmail.com>
2023-07-06 13:00:38 +0330

    revised files.

--- feloopy/algorithms/heuristic/GWO.py
+++ feloopy/algorithms/heuristic/GWO.py
@@ -1,12 +1,13 @@
 '''
  # @ Author: Keivan Tafakkori
  # @ Created: 2023-05-11
- # @ Modified: 2023-05-12
+ # @ Modified: 2023-07-06
  # @ Contact: https://www.linkedin.com/in/keivan-tafakkori/
  # @ Github: https://github.com/ktafakkori
  # @ Website: https://ktafakkori.github.io/
  # @ Copyright: 2023. MIT License. All Rights Reserved.
  '''
+
 
 import numpy as np
 import warnings as wn
