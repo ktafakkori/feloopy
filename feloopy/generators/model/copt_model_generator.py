18b95179b422250995cd84d76de9bfc77ca448c0
ktafakkori <k.tafakkori@gmail.com>
2023-09-18 11:56:39 +0330

    revised files.

--- feloopy/generators/model/copt_model_generator.py
+++ feloopy/generators/model/copt_model_generator.py
@@ -1,5 +1,8 @@
-from coptpy import *
+import coptpy as copt
+
+envconfig = copt.EnvrConfig()
+envconfig.set('nobanner', '1')
+env = copt.Envr(envconfig)
 
 def generate_model(features):
-    env = Envr()
     return env.createModel(features['model_name'])
