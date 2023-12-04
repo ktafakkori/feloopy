150153bdd1bec14c434fe4e1f766cfa53d5b6511
ktafakkori <k.tafakkori@gmail.com>
2023-12-04 15:04:59 +0330

    revised files.

--- feloopy/cli.py
+++ feloopy/cli.py
@@ -1,8 +1,22 @@
 import sys
+import argparse
 from .clitools import detect_package_manager
 
+__version__ = "0.2.8"
 
 def main():
+    
+    parser = argparse.ArgumentParser(description="FelooPy's command-line tool")
+    parser.add_argument("--detect", action="store_true", help="Detect the package manager")
+    parser.add_argument("--version", action="store_true", help="Display the version of feloopy")
 
-    if len(sys.argv) == 2 and sys.argv[1] == "detect":
-        detect_package_manager()
+    args = parser.parse_args()
+
+    if args.detect:
+        detect_package_manager(verbose=True)
+        
+    elif args.version:
+        print(f"FelooPy (v{__version__})")
+        
+    else:
+        print("No valid command provided. Use --detect or --version.")
--- feloopy/clitools/pkg_manager_detector.py
+++ feloopy/clitools/pkg_manager_detector.py
@@ -1,14 +1,14 @@
 import subprocess
 import sys
 
-def detect_package_manager():
+def detect_package_manager(verbose):
     
     common_package_managers = ['apt-get', 'dnf', 'yum', 'pacman', 'zypper', 'apk']
 
     for cmd in common_package_managers:
         try:
             subprocess.run(['which', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-            print(f"found the {cmd} package manager.")
+            if verbose: print(f"found the {cmd} package manager.")
             return cmd
         except subprocess.CalledProcessError:
             pass
@@ -16,7 +16,7 @@ def detect_package_manager():
     if sys.platform.startswith('win'):
         try:
             subprocess.run(['choco', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-            print("Found Chocolatey package manager.")
+            if verbose: print("Found Chocolatey package manager.")
             return 'choco'
         except subprocess.CalledProcessError:
             pass
@@ -24,10 +24,10 @@ def detect_package_manager():
     if sys.platform.startswith('darwin'):
         try:
             subprocess.run(['brew', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-            print("Found Homebrew package manager.")
+            if verbose: print("Found Homebrew package manager.")
             return 'brew'
         except subprocess.CalledProcessError:
             pass
 
-    print("None of the common package managers found. Exiting installation.")
+    if verbose: print("None of the common package managers found. Exiting installation.")
     sys.exit(1)
