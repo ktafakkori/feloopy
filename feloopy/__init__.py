442407fb413e23811d4a1e9ae90006910cc3ebf0
ktafakkori <k.tafakkori@gmail.com>
2024-04-08 00:15:43 +0330

    revised files.

--- feloopy/__init__.py
+++ feloopy/__init__.py
@@ -1,9 +1,6 @@
 # Copyright (c) 2022-2024, Keivan Tafakkori. All rights reserved.
 # See the file LICENSE file for licensing details.
 
-
-#import pretty_errors
-
 try:
     from .extras import *
 except ImportError:
